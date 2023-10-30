3ae6694cd017347c89e7830a1d1051e5a0b8a4f9
Rodrigo Camargo <103613954+Rodrigo-001@users.noreply.github.com>
2023-10-30 14:15:33 +0000

    Update 6 files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./styles.css";
 
 function App() {
   const [joke, setJoke] = useState("");
   const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
+    // armazena piadas favoritas no localStorage como uma sequência JSON. Depois, recupera e analisa os dados do localStorage.
     const storedFavorites = JSON.parse(localStorage.getItem("favoriteJokes"));
     if (storedFavorites) {
       setFavorites(storedFavorites);
     }
   }, []);
 
+  // função fazendo uma chamada assíncrona
   const fetchJoke = async () => {
     try {
+      // await - espere que a api(axios.get) retorne uma piada
       const response = await axios.get(
         "https://api.chucknorris.io/jokes/random",
       );
+      // atualiza o estado joke com a piada obtida ( re-renderização)
+      // data - prop que contém os dados retornados pelo servidor.
+      // value -  prop específica que contém o valor da piada.
       setJoke(response.data.value);
+      // caso erro na busca da piada, trata exceção
     } catch (error) {
       console.error("Erro ao buscar piada:", error);
     }
@@ -44,10 +52,9 @@ function App() {
   return (
     <div>
       <h1>Chuck Norris Joke</h1>
-      <button onClick={fetchJoke}>Nova Piada</button>
+      <button onClick={fetchJoke}>New joke</button>
       <p>{joke}</p>
       <button onClick={handleLike}>Like</button>
-
       <h2>Favoritos</h2>
       <ul>
         {favorites.map((favorite, index) => (
@@ -57,6 +64,9 @@ function App() {
           </li>
         ))}
       </ul>
+      <br></br>
+      <br></br>
+      <p>author: Rodrigo Camargo</p>
     </div>
   );
 }
